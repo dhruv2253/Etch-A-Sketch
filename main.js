3712efb06da91c0f8433096c3a2cab70b468c7d7
dhruv2253 <dhruv225@outlook.com>
2022-10-26 17:24:40 -0400

    refactoring and centering items on page

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
 const container = document.querySelector('.container');
 const clearButton = document.querySelector('.clear');
-
+const sizeButton = document.querySelector('.size');
 
 function createBoxes(numBox){
     container.style.gridTemplateColumns = `repeat(${numBox}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${numBox}, 1fr)`;
     for(let i = 0;i < numBox*numBox;i++){
         const square = document.createElement('div');
         square.id = 'grid-square'
         container.appendChild(square);
-        square.addEventListener('mouseenter', (e) =>
-        e.target.style.backgroundColor = 'black');
-        
-        clearButton.addEventListener('click', (e) =>
-        square.style.backgroundColor = 'white');
-        
-        console.log(numBox);
     }
+    let gridSquares = container.querySelectorAll('div');
+    gridSquares.forEach(square => square.addEventListener('mouseover', (e) =>
+    e.target.style.backgroundColor = 'black'))
 }
 
-createBoxes(16);
+function clear() {
+    let gridSquares = container.querySelectorAll('div');
+    gridSquares.forEach(square => square.style.backgroundColor = 'white');
+}
+
+document.onload = createBoxes(16);
 
 
 
 
+
+
+
+sizeButton.addEventListener('click', (e) => {
+    const prompt = window.prompt("Enter number of squares per side.");
+    clearButton.addEventListener('click', clear);
+    createBoxes(prompt);
+})
+
+
+clearButton.addEventListener('click', clear);
 
     
     
