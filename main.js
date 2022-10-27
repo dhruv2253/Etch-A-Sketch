80dc821c4f46fa4b3da5a6459351d6579685eb44
dhruv2253 <dhruv225@outlook.com>
2022-10-27 01:58:33 -0400

    take out clear() in buttons. add touch events

--- main.js
+++ main.js
@@ -43,8 +43,8 @@ document.onload = createBoxes(16);
 // Button to change color to black event
 blackButton.addEventListener('click', () => {
     let gridSquares = container.querySelectorAll('div');
-    // Clear board
-    clear();
+   
+   
     for (const square of gridSquares) {
         square.addEventListener('mouseover', () => {
             // Set square color to black and opacity to 100% when mouse enters the square
@@ -58,8 +58,8 @@ blackButton.addEventListener('click', () => {
 // Button to change color to greyscale event
 greyButton.addEventListener('click', () => {
     let gridSquares = container.querySelectorAll('div');
-    // Clear board
-    clear();
+   
+    
     for (const square of gridSquares) {
         //opacity variable
         let op = .1;
@@ -78,7 +78,7 @@ greyButton.addEventListener('click', () => {
 rainbowButton.addEventListener('click', () => {
     let gridSquares = container.querySelectorAll('div');
     // Clear board
-    clear();
+    
     for (const square of gridSquares) {
         // Function to get random number between a range
         const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
@@ -101,21 +101,98 @@ sizeForm.addEventListener('submit', (e) => {
     const size = document.querySelector('#size');
     e.preventDefault();
     clear();
-    if (typeof size.value != Number || size.value>250){
-        size.value = "Invalid Input"
-        
+
+    if (!parseInt(size.value)  || size.value>250){
+        size.value = "Invalid Input";
+        console.log(typeof size.value);
         size.addEventListener('focus', () => {
             size.value = ""
         })
     }
-    createBoxes(size);
-    console.log(size);
+    createBoxes(size.value);
 });
 
 // Clear board when button is clicked
 clearButton.addEventListener('click', clear);
 
+
+// Mobile event listeners
+
+
+// Button to change color to black event
+blackButton.addEventListener('touchend', () => {
+    let gridSquares = container.querySelectorAll('div');
+    // Clear board
+   
+    for (const square of gridSquares) {
+        square.addEventListener('touchmove', () => {
+            // Set square color to black and opacity to 100% when mouse enters the square
+            square.style.backgroundColor = 'black';
+            square.style.opacity = '100%';
+        });
+    }
     
+});
+
+// Button to change color to greyscale event
+greyButton.addEventListener('touchend', () => {
+    let gridSquares = container.querySelectorAll('div');
+    // Clear board
+    
+    for (const square of gridSquares) {
+        //opacity variable
+        let op = .1;
+        square.addEventListener('touchmove', () => {
+            // Set square color to black and add 10% to opacity of square each time mouse enters the square
+            square.style.backgroundColor = 'black';
+            square.style.opacity = `${op}`;
+            // add .1 to opacity
+            op += .1
+
+        });
+    }
+});
+
+// Button to change color to rainbow event
+rainbowButton.addEventListener('touchend', () => {
+    let gridSquares = container.querySelectorAll('div');
+    // Clear board
+    
+    for (const square of gridSquares) {
+        // Function to get random number between a range
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        // set the random number to rgb value
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        const rgb = `rgb(${r},${g},${b})`;
+
+        square.addEventListener('touchmove', () => {
+            // change square color to a random color
+            square.style.backgroundColor = rgb;
+            square.style.opacity = '100%'
+        });
+    }
+});
+
+// Change size of the grid based on user input
+sizeForm.addEventListener('submit', (e) => {
+    const size = document.querySelector('#size');
+    e.preventDefault();
+    clear();
+
+    if (!parseInt(size.value)  || size.value>250){
+        size.value = "Invalid Input";
+        console.log(typeof size.value);
+        size.addEventListener('focus', () => {
+            size.value = ""
+        })
+    }
+    createBoxes(size.value);
+});
+
+// Clear board when button is clicked
+clearButton.addEventListener('touchend', clear); 
     
 
 
