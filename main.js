f8fee195a300099a9943c3dce3b5c9e32684dd0c
dhruv2253 <dhruv225@outlook.com>
2022-10-26 23:29:38 -0400

    rainbow button/func and button for black/func

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const container = document.querySelector('.container');
 const clearButton = document.querySelector('.clear');
 const sizeButton = document.querySelector('.size');
-
+const rainbowButton = document.querySelector('.rainbow');
+const greyButton = document.querySelector('.greyscale');
+const blackButton = document.querySelector('.black');
 function createBoxes(numBox){
     container.style.gridTemplateColumns = `repeat(${numBox}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${numBox}, 1fr)`;
@@ -11,8 +13,8 @@ function createBoxes(numBox){
         container.appendChild(square);
     }
     let gridSquares = container.querySelectorAll('div');
-    gridSquares.forEach(square => square.addEventListener('mouseover', (e) =>
-    e.target.style.backgroundColor = 'black'))
+    gridSquares.forEach(square => square.addEventListener('mouseover', () =>
+    square.style.backgroundColor = 'black'));
 }
 
 function clear() {
@@ -25,7 +27,36 @@ document.onload = createBoxes(16);
 
 
 
+blackButton.addEventListener('click', () => {
+    let gridSquares = container.querySelectorAll('div');
+    clear();
+    for (const square of gridSquares) {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+    }
+    
+});
 
+greyButton.addEventListener('click', () => {
+
+});
+
+rainbowButton.addEventListener('click', (e) => {
+    let gridSquares = container.querySelectorAll('div');
+        clear();
+    for (const square of gridSquares) {
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        const rgb = `rgb(${r},${g},${b})`;
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = rgb;
+        });
+    }
+    
+});
 
 
 sizeButton.addEventListener('click', (e) => {
